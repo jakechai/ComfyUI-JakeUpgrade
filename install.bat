4b727015dd67367ade797f1c60b26bdea1f4d7cf
jakechai <kefuchai@qq.com>
2024-07-03 10:31:00 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,14 +2,8 @@
 
 echo Installing ComfyUI's JakeUpgrade Nodes with ComfyUI Portable..
 
-REM python path
-set "python_exec="E:\Program Files\ComfyUI_windows_portable\python_embeded\python.exe""
+set "python_exec="../../../python_embeded/python.exe""
 
-%python_exec% -s -m pip install opencv-python
-%python_exec% -s -m pip install piexif
-%python_exec% -s -m pip install torch
-%python_exec% -s -m pip install numpy<2
-%python_exec% -s -m pip install numexpr
-%python_exec% -s -m pip install simpleeval
+%python_exec% -s -m pip install -r requirements.txt
 
 pause
