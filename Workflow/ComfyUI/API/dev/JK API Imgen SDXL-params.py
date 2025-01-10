bd3f71e6ad06e72dc8014ea88074baed5e3dade8
jakechai <kefuchai@qq.com>
2025-01-10 21:40:28 +0800

    v1.8.7
    fix api params

--- Workflow/ComfyUI/API/dev/JK API Imgen SDXL-params.py
+++ Workflow/ComfyUI/API/dev/JK API Imgen SDXL-params.py
@@ -1,255 +1,254 @@
-'''
-["1"]["inputs"]["ckpt_name"] = 
-["2"]["inputs"]["vae_name"] = 
-["4"]["inputs"]["custom_width"] = 
-["4"]["inputs"]["custom_height"] = 
-# positive l
-["6:14"]["inputs"]["string"] = 
-# positive g
-["6:15"]["inputs"]["string"] = 
-# negative l
-["6:16"]["inputs"]["string"] = 
-# negative g
-["6:17"]["inputs"]["string"] = 
-# sdxl target res
-["6:18"]["inputs"]["target_res_scale"] = 
-# switch clip l g
-["6:19"]["inputs"]["boolean_value"] = 
-# negative clip g
-["6:20"]["inputs"]["boolean_value"] = 
-["6:21"]["inputs"]["cfg"] = 
-["6:22"]["inputs"]["sampler_name"] = 
-["6:23"]["inputs"]["scheduler"] = 
-["6:23"]["inputs"]["steps"] = 
-["6:23"]["inputs"]["denoise"] = 
-# seed
-["5"]["inputs"]["value"] = 
-["6:24"]["inputs"]["noise_mode"] = 
-["6:24"]["inputs"]["batch_seed_mode"] = 
-["6:24"]["inputs"]["variation_seed"] = 
-["6:24"]["inputs"]["variation_strength"] = 
-["6:24"]["inputs"]["variation_method"] = 
-# detail daemon
-["6:25"]["inputs"]["boolean_value"] = 
-["6:26"]["inputs"]["detail_amount"] = 
-["6:26"]["inputs"]["start"] = 
-["6:26"]["inputs"]["end"] = 
-["6:26"]["inputs"]["bias"] = 
-["6:26"]["inputs"]["exponent"] = 
-["6:26"]["inputs"]["start_offset"] = 
-["6:26"]["inputs"]["end_offset"] = 
-["6:26"]["inputs"]["fade"] = 
-["6:26"]["inputs"]["smooth"] = 
-# img2img
-["6:27"]["inputs"]["boolean_value"] = 
-["3"]["inputs"]["image"] = 
-["6:28"]["inputs"]["resize_mode"] = 
-# repaint latent
-["6:29"]["inputs"]["boolean_value"] = 
-# inpaint ckpt
-["6:30"]["inputs"]["boolean_value"] = 
-["6:31"]["inputs"]["noise_mask"] = 
-# blend with original image
-["6:32"]["inputs"]["boolean_value"] = 
-# blend using origin mask
-["6:34"]["inputs"]["boolean_value"] = 
-# blend mask dilation
-["6:35"]["inputs"]["dilation"] = 
-# blend mask blur
-["6:36"]["inputs"]["amount"] = 
-# latent fill
-["6:37"]["inputs"]["boolean_value"] = 
-# image fill
-["6:38"]["inputs"]["boolean_value"] = 
-# image fill resize
-["6:39"]["inputs"]["resize_mode"] = 
-# image fill tile
-["6:40"]["inputs"]["boolean_value"] = 
-["6:41"]["inputs"]["width"] = 
-["6:41"]["inputs"]["height"] = 
-# input mask
-["6:42"]["inputs"]["boolean_value"] = 
-["6:43"]["inputs"]["resize_mode"] = 
-# Mask Detail Removal Threshold
-["6:44"]["inputs"]["value"] = 
-# mask detail removal invert removal
-["6:45"]["inputs"]["boolean_value"] = 
-# input mask invert
-["6:46"]["inputs"]["boolean_value"] = 
-# Pad left
-["6:47"]["inputs"]["value"] = 
-# Pad top
-["6:48"]["inputs"]["value"] = 
-# Pad right
-["6:49"]["inputs"]["value"] = 
-# Pad bottom
-["6:50"]["inputs"]["value"] = 
-# mask dilation
-["6:52"]["inputs"]["dilation"] = 
-# mask blur
-["6:53"]["inputs"]["amount"] = 
-# mask binary
-["6:54"]["inputs"]["boolean_value"] = 
-["7"]["inputs"]["inpaint_crop_and_stitch"] = 
-# latent crop
-["6:56"]["inputs"]["boolean_value"] = 
-# ignore latent crop offset
-["6:57"]["inputs"]["boolean_value"] = 
-["7"]["inputs"]["padding"] = 
-["7"]["inputs"]["use_image_res"] = 
-["7"]["inputs"]["use_target_res"] = 
-["7"]["inputs"]["target_res"] = 
-["7"]["inputs"]["use_target_mega_pixel"] = 
-["7"]["inputs"]["target_mega_pixel"] = 
-# color grading
-["6:59"]["inputs"]["boolean_value"] = 
-["6:60"]["inputs"]["brightness"] = 
-["6:60"]["inputs"]["contrast"] = 
-["6:60"]["inputs"]["saturation"] = 
-["6:60"]["inputs"]["R"] = 
-["6:60"]["inputs"]["G"] = 
-["6:60"]["inputs"]["B"] = 
-["6:61"]["inputs"]["image_upscale_method"] = 
-["6:61"]["inputs"]["latent_upscale_method"] = 
-# batch size
-["6:64"]["inputs"]["amount"] = 
-# stop at clip layer
-["6:9"]["inputs"]["boolean_value"] = 
-["6:10":]["inputs"]["stop_at_clip_layer"] = 
-# tiling
-["6:11"]["inputs"]["boolean_value"] = 
-["6:12"]["inputs"]["tiling"] = 
-# kohya
-["8"]["inputs"]["boolean_value"] = 
-["9"]["inputs"]["downscale_factor"] = 
-["9"]["inputs"]["start_percent"] = 
-["9"]["inputs"]["end_percent"] = 
-# sag
-["10"]["inputs"]["boolean_value"] = 
-["11"]["inputs"]["scale"] = 
-# freeu
-["12"]["inputs"]["boolean_value"] = 
-# lcm
-["14"]["inputs"]["boolean_value"] = 
-["15"]["inputs"]["sampling"] = 
-# real cfg
-["16"]["inputs"]["boolean_value"] = 
-["17"]["inputs"]["multiplier"] = 
-# lora 1-4
-["18"]["inputs"]["lora_name_1"] = 
-["18"]["inputs"]["lora_weight_1"] = 
-["18"]["inputs"]["lora_name_2"] = 
-["18"]["inputs"]["lora_weight_2"] = 
-["18"]["inputs"]["lora_name_3"] = 
-["18"]["inputs"]["lora_weight_3"] = 
-["18"]["inputs"]["lora_name_4"] = 
-["18"]["inputs"]["lora_weight_4"] = 
-# lora 5-8
-["19"]["inputs"]["lora_name_1"] = 
-["19"]["inputs"]["lora_weight_1"] = 
-["19"]["inputs"]["lora_name_2"] = 
-["19"]["inputs"]["lora_weight_2"] = 
-["19"]["inputs"]["lora_name_3"] = 
-["19"]["inputs"]["lora_weight_3"] = 
-["19"]["inputs"]["lora_name_4"] = 
-["19"]["inputs"]["lora_weight_4"] = 
-# ipa 1
-["20"]["inputs"]["preset"] = 
-["21"]["inputs"]["image"] = 
-["22"]["inputs"]["weight"] = 
-["22"]["inputs"]["weight_type"] = 
-["22"]["inputs"]["combine_embeds"] = 
-["22"]["inputs"]["start_at"] = 
-["22"]["inputs"]["end_at"] = 
-["22"]["inputs"]["embeds_scaling"] = 
-# ipa 2
-["23"]["inputs"]["preset"] = 
-["24"]["inputs"]["image"] = 
-["25"]["inputs"]["weight"] = 
-["25"]["inputs"]["weight_type"] = 
-["25"]["inputs"]["combine_embeds"] = 
-["25"]["inputs"]["start_at"] = 
-["25"]["inputs"]["end_at"] = 
-["25"]["inputs"]["embeds_scaling"] = 
-# cn res
-["26"]["inputs"]["value"] = 
-# cn effective mask
-["6:13"]["inputs"]["boolean_value"] = 
-# cn 0
-["27"]["inputs"]["control_net_name"] = 
-["27"]["inputs"]["union_type"] = 
-# CN 0 custom img
-["28"]["inputs"]["boolean_value"] = 
-["29"]["inputs"]["image"] = 
-["30"]["inputs"]["resize_mode"] = 
-# CN 0 inpaint
-["31"]["inputs"]["boolean_value"] = 
-["32"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_0"] = 
-["63"]["inputs"]["start_percent_0"] = 
-["63"]["inputs"]["end_percent_0"] = 
-# cn 1
-["33"]["inputs"]["control_net_name"] = 
-["33"]["inputs"]["union_type"] = 
-# CN 1 custom img
-["34"]["inputs"]["boolean_value"] = 
-["35"]["inputs"]["image"] = 
-["36"]["inputs"]["resize_mode"] = 
-# CN 1 inpaint
-["37"]["inputs"]["boolean_value"] = 
-["38"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_1"] = 
-["63"]["inputs"]["start_percent_1"] = 
-["63"]["inputs"]["end_percent_1"] = 
-# cn 2
-["39"]["inputs"]["control_net_name"] = 
-["39"]["inputs"]["union_type"] = 
-# CN 2 custom img
-["40"]["inputs"]["boolean_value"] = 
-["41"]["inputs"]["image"] = 
-["42"]["inputs"]["resize_mode"] = 
-# CN 2 inpaint
-["43"]["inputs"]["boolean_value"] = 
-["44"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_2"] = 
-["63"]["inputs"]["start_percent_2"] = 
-["63"]["inputs"]["end_percent_2"] = 
-# cn 3
-["45"]["inputs"]["control_net_name"] = 
-["45"]["inputs"]["union_type"] = 
-# CN 3 custom img
-["46"]["inputs"]["boolean_value"] = 
-["47"]["inputs"]["image"] = 
-["48"]["inputs"]["resize_mode"] = 
-# CN 3 inpaint
-["49"]["inputs"]["boolean_value"] = 
-["50"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_3"] = 
-["63"]["inputs"]["start_percent_3"] = 
-["63"]["inputs"]["end_percent_3"] = 
-# cn 4
-["51"]["inputs"]["control_net_name"] = 
-["51"]["inputs"]["union_type"] = 
-# CN 4 custom img
-["52"]["inputs"]["boolean_value"] = 
-["53"]["inputs"]["image"] = 
-["54"]["inputs"]["resize_mode"] = 
-# CN 4 inpaint
-["55"]["inputs"]["boolean_value"] = 
-["56"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_4"] = 
-["63"]["inputs"]["start_percent_4"] = 
-["63"]["inputs"]["end_percent_4"] = 
-# cn 5
-["57"]["inputs"]["control_net_name"] = 
-["57"]["inputs"]["union_type"] = 
-# CN 5 custom img
-["58"]["inputs"]["boolean_value"] = 
-["59"]["inputs"]["image"] = 
-["60"]["inputs"]["resize_mode"] = 
-# CN 5 inpaint
-["61"]["inputs"]["boolean_value"] = 
-["62"]["inputs"]["preprocessor"] = 
-["63"]["inputs"]["controlnet_strength_5"] = 
-["63"]["inputs"]["start_percent_5"] = 
-["63"]["inputs"]["end_percent_5"] = 
+# ["1"]["inputs"]["ckpt_name"] = 
+# ["2"]["inputs"]["vae_name"] = 
+# ["4"]["inputs"]["custom_width"] = 
+# ["4"]["inputs"]["custom_height"] = 
+# # positive l
+# ["6:14"]["inputs"]["string"] = 
+# # positive g
+# ["6:15"]["inputs"]["string"] = 
+# # negative l
+# ["6:16"]["inputs"]["string"] = 
+# # negative g
+# ["6:17"]["inputs"]["string"] = 
+# # sdxl target res
+# ["6:18"]["inputs"]["target_res_scale"] = 
+# # switch clip l g
+# ["6:19"]["inputs"]["boolean_value"] = 
+# # negative clip g
+# ["6:20"]["inputs"]["boolean_value"] = 
+# ["6:21"]["inputs"]["cfg"] = 
+# ["6:22"]["inputs"]["sampler_name"] = 
+# ["6:23"]["inputs"]["scheduler"] = 
+# ["6:23"]["inputs"]["steps"] = 
+# ["6:23"]["inputs"]["denoise"] = 
+# # seed
+# ["5"]["inputs"]["value"] = 
+# ["6:24"]["inputs"]["noise_mode"] = 
+# ["6:24"]["inputs"]["batch_seed_mode"] = 
+# ["6:24"]["inputs"]["variation_seed"] = 
+# ["6:24"]["inputs"]["variation_strength"] = 
+# ["6:24"]["inputs"]["variation_method"] = 
+# # detail daemon
+# ["6:25"]["inputs"]["boolean_value"] = 
+# ["6:26"]["inputs"]["detail_amount"] = 
+# ["6:26"]["inputs"]["start"] = 
+# ["6:26"]["inputs"]["end"] = 
+# ["6:26"]["inputs"]["bias"] = 
+# ["6:26"]["inputs"]["exponent"] = 
+# ["6:26"]["inputs"]["start_offset"] = 
+# ["6:26"]["inputs"]["end_offset"] = 
+# ["6:26"]["inputs"]["fade"] = 
+# ["6:26"]["inputs"]["smooth"] = 
+# # img2img
+# ["6:27"]["inputs"]["boolean_value"] = 
+# ["3"]["inputs"]["image"] = 
+# ["6:28"]["inputs"]["resize_mode"] = 
+# # repaint latent
+# ["6:29"]["inputs"]["boolean_value"] = 
+# # inpaint ckpt
+# ["6:30"]["inputs"]["boolean_value"] = 
+# ["6:31"]["inputs"]["noise_mask"] = 
+# # blend with original image
+# ["6:32"]["inputs"]["boolean_value"] = 
+# # blend using origin mask
+# ["6:34"]["inputs"]["boolean_value"] = 
+# # blend mask dilation
+# ["6:35"]["inputs"]["dilation"] = 
+# # blend mask blur
+# ["6:36"]["inputs"]["amount"] = 
+# # latent fill
+# ["6:37"]["inputs"]["boolean_value"] = 
+# # image fill
+# ["6:38"]["inputs"]["boolean_value"] = 
+# # image fill resize
+# ["6:39"]["inputs"]["resize_mode"] = 
+# # image fill tile
+# ["6:40"]["inputs"]["boolean_value"] = 
+# ["6:41"]["inputs"]["width"] = 
+# ["6:41"]["inputs"]["height"] = 
+# # input mask
+# ["6:42"]["inputs"]["boolean_value"] = 
+# ["6:43"]["inputs"]["resize_mode"] = 
+# # Mask Detail Removal Threshold
+# ["6:44"]["inputs"]["value"] = 
+# # mask detail removal invert removal
+# ["6:45"]["inputs"]["boolean_value"] = 
+# # input mask invert
+# ["6:46"]["inputs"]["boolean_value"] = 
+# # Pad left
+# ["6:47"]["inputs"]["value"] = 
+# # Pad top
+# ["6:48"]["inputs"]["value"] = 
+# # Pad right
+# ["6:49"]["inputs"]["value"] = 
+# # Pad bottom
+# ["6:50"]["inputs"]["value"] = 
+# # mask dilation
+# ["6:52"]["inputs"]["dilation"] = 
+# # mask blur
+# ["6:53"]["inputs"]["amount"] = 
+# # mask binary
+# ["6:54"]["inputs"]["boolean_value"] = 
+# ["7"]["inputs"]["inpaint_crop_and_stitch"] = 
+# # latent crop
+# ["6:56"]["inputs"]["boolean_value"] = 
+# # ignore latent crop offset
+# ["6:57"]["inputs"]["boolean_value"] = 
+# ["7"]["inputs"]["padding"] = 
+# ["7"]["inputs"]["use_image_res"] = 
+# ["7"]["inputs"]["use_target_res"] = 
+# ["7"]["inputs"]["target_res"] = 
+# ["7"]["inputs"]["use_target_mega_pixel"] = 
+# ["7"]["inputs"]["target_mega_pixel"] = 
+# # color grading
+# ["6:59"]["inputs"]["boolean_value"] = 
+# ["6:60"]["inputs"]["brightness"] = 
+# ["6:60"]["inputs"]["contrast"] = 
+# ["6:60"]["inputs"]["saturation"] = 
+# ["6:60"]["inputs"]["R"] = 
+# ["6:60"]["inputs"]["G"] = 
+# ["6:60"]["inputs"]["B"] = 
+# ["6:61"]["inputs"]["image_upscale_method"] = 
+# ["6:61"]["inputs"]["latent_upscale_method"] = 
+# # batch size
+# ["6:64"]["inputs"]["amount"] = 
+# # stop at clip layer
+# ["6:9"]["inputs"]["boolean_value"] = 
+# ["6:10":]["inputs"]["stop_at_clip_layer"] = 
+# # tiling
+# ["6:11"]["inputs"]["boolean_value"] = 
+# ["6:12"]["inputs"]["tiling"] = 
+# # kohya
+# ["8"]["inputs"]["boolean_value"] = 
+# ["9"]["inputs"]["downscale_factor"] = 
+# ["9"]["inputs"]["start_percent"] = 
+# ["9"]["inputs"]["end_percent"] = 
+# # sag
+# ["10"]["inputs"]["boolean_value"] = 
+# ["11"]["inputs"]["scale"] = 
+# # freeu
+# ["12"]["inputs"]["boolean_value"] = 
+# # lcm
+# ["14"]["inputs"]["boolean_value"] = 
+# ["15"]["inputs"]["sampling"] = 
+# # real cfg
+# ["16"]["inputs"]["boolean_value"] = 
+# ["17"]["inputs"]["multiplier"] = 
+# # lora 1-4
+# ["18"]["inputs"]["lora_name_1"] = 
+# ["18"]["inputs"]["lora_weight_1"] = 
+# ["18"]["inputs"]["lora_name_2"] = 
+# ["18"]["inputs"]["lora_weight_2"] = 
+# ["18"]["inputs"]["lora_name_3"] = 
+# ["18"]["inputs"]["lora_weight_3"] = 
+# ["18"]["inputs"]["lora_name_4"] = 
+# ["18"]["inputs"]["lora_weight_4"] = 
+# # lora 5-8
+# ["19"]["inputs"]["lora_name_1"] = 
+# ["19"]["inputs"]["lora_weight_1"] = 
+# ["19"]["inputs"]["lora_name_2"] = 
+# ["19"]["inputs"]["lora_weight_2"] = 
+# ["19"]["inputs"]["lora_name_3"] = 
+# ["19"]["inputs"]["lora_weight_3"] = 
+# ["19"]["inputs"]["lora_name_4"] = 
+# ["19"]["inputs"]["lora_weight_4"] = 
+# # ipa 1
+# ["20"]["inputs"]["preset"] = 
+# ["21"]["inputs"]["image"] = 
+# ["22"]["inputs"]["weight"] = 
+# ["22"]["inputs"]["weight_type"] = 
+# ["22"]["inputs"]["combine_embeds"] = 
+# ["22"]["inputs"]["start_at"] = 
+# ["22"]["inputs"]["end_at"] = 
+# ["22"]["inputs"]["embeds_scaling"] = 
+# # ipa 2
+# ["23"]["inputs"]["preset"] = 
+# ["24"]["inputs"]["image"] = 
+# ["25"]["inputs"]["weight"] = 
+# ["25"]["inputs"]["weight_type"] = 
+# ["25"]["inputs"]["combine_embeds"] = 
+# ["25"]["inputs"]["start_at"] = 
+# ["25"]["inputs"]["end_at"] = 
+# ["25"]["inputs"]["embeds_scaling"] = 
+# # cn res
+# ["26"]["inputs"]["value"] = 
+# # cn effective mask
+# ["6:13"]["inputs"]["boolean_value"] = 
+# # cn 0
+# ["27"]["inputs"]["control_net_name"] = 
+# ["27"]["inputs"]["union_type"] = 
+# # CN 0 custom img
+# ["28"]["inputs"]["boolean_value"] = 
+# ["29"]["inputs"]["image"] = 
+# ["30"]["inputs"]["resize_mode"] = 
+# # CN 0 inpaint
+# ["31"]["inputs"]["boolean_value"] = 
+# ["32"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_0"] = 
+# ["63"]["inputs"]["start_percent_0"] = 
+# ["63"]["inputs"]["end_percent_0"] = 
+# # cn 1
+# ["33"]["inputs"]["control_net_name"] = 
+# ["33"]["inputs"]["union_type"] = 
+# # CN 1 custom img
+# ["34"]["inputs"]["boolean_value"] = 
+# ["35"]["inputs"]["image"] = 
+# ["36"]["inputs"]["resize_mode"] = 
+# # CN 1 inpaint
+# ["37"]["inputs"]["boolean_value"] = 
+# ["38"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_1"] = 
+# ["63"]["inputs"]["start_percent_1"] = 
+# ["63"]["inputs"]["end_percent_1"] = 
+# # cn 2
+# ["39"]["inputs"]["control_net_name"] = 
+# ["39"]["inputs"]["union_type"] = 
+# # CN 2 custom img
+# ["40"]["inputs"]["boolean_value"] = 
+# ["41"]["inputs"]["image"] = 
+# ["42"]["inputs"]["resize_mode"] = 
+# # CN 2 inpaint
+# ["43"]["inputs"]["boolean_value"] = 
+# ["44"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_2"] = 
+# ["63"]["inputs"]["start_percent_2"] = 
+# ["63"]["inputs"]["end_percent_2"] = 
+# # cn 3
+# ["45"]["inputs"]["control_net_name"] = 
+# ["45"]["inputs"]["union_type"] = 
+# # CN 3 custom img
+# ["46"]["inputs"]["boolean_value"] = 
+# ["47"]["inputs"]["image"] = 
+# ["48"]["inputs"]["resize_mode"] = 
+# # CN 3 inpaint
+# ["49"]["inputs"]["boolean_value"] = 
+# ["50"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_3"] = 
+# ["63"]["inputs"]["start_percent_3"] = 
+# ["63"]["inputs"]["end_percent_3"] = 
+# # cn 4
+# ["51"]["inputs"]["control_net_name"] = 
+# ["51"]["inputs"]["union_type"] = 
+# # CN 4 custom img
+# ["52"]["inputs"]["boolean_value"] = 
+# ["53"]["inputs"]["image"] = 
+# ["54"]["inputs"]["resize_mode"] = 
+# # CN 4 inpaint
+# ["55"]["inputs"]["boolean_value"] = 
+# ["56"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_4"] = 
+# ["63"]["inputs"]["start_percent_4"] = 
+# ["63"]["inputs"]["end_percent_4"] = 
+# # cn 5
+# ["57"]["inputs"]["control_net_name"] = 
+# ["57"]["inputs"]["union_type"] = 
+# # CN 5 custom img
+# ["58"]["inputs"]["boolean_value"] = 
+# ["59"]["inputs"]["image"] = 
+# ["60"]["inputs"]["resize_mode"] = 
+# # CN 5 inpaint
+# ["61"]["inputs"]["boolean_value"] = 
+# ["62"]["inputs"]["preprocessor"] = 
+# ["63"]["inputs"]["controlnet_strength_5"] = 
+# ["63"]["inputs"]["start_percent_5"] = 
+# ["63"]["inputs"]["end_percent_5"] = 
