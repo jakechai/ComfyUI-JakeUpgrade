ade7bd7d56f949a63f3f79212308a3a1eaafe46e
jakechai <kefuchai@qq.com>
2025-09-18 17:25:34 +0800

    upload api workflows

---
+++ Workflow/ComfyUI/API/dev/JK API Refine QWen-params.py
@@ -0,0 +1,54 @@
+# ["1"]["inputs"]["unet_name"] = 
+# ["1"]["inputs"]["weight_dtype"] = 
+# ["2"]["inputs"]["clip_name"] = 
+# ["24"]["inputs"]["vae_name"] = 
+# ["3"]["inputs"]["image"] = 
+# cn effective mask
+# ["4:345"]["inputs"]["boolean_value"] = 
+# cn lora
+# ["6:376"]["inputs"]["boolean_value"] = 
+# # positive
+# ["4:386"]["inputs"]["text"] = 
+# # negative
+# ["4:341"]["inputs"]["text"] = 
+# ["4:389"]["inputs"]["shift"] = 
+# ["4:385"]["inputs"]["cfg"] = 
+# ["4:347"]["inputs"]["sampler_name"] = 
+# ["4:384"]["inputs"]["scheduler"] = 
+# ["4:384"]["inputs"]["steps"] = 
+# ["4:384"]["inputs"]["denoise"] = 
+# ["4:382"]["inputs"]["noise_seed"] = 
+# # detail daemon
+# ["4:353"]["inputs"]["boolean_value"] = 
+# ["4:344"]["inputs"]["dishonesty_factor"] = 
+# ["4:344"]["inputs"]["start_percent"] = 
+# ["4:344"]["inputs"]["end_percent"] = 
+# # batch size
+# ["4:349"]["inputs"]["amount"] = 
+# # lora 1-4
+# ["13"]["inputs"]["lora_name_1"] = 
+# ["13"]["inputs"]["model_weight_1"] = 
+# ["13"]["inputs"]["lora_name_2"] = 
+# ["13"]["inputs"]["model_weight_2"] = 
+# ["13"]["inputs"]["lora_name_3"] = 
+# ["13"]["inputs"]["model_weight_3"] = 
+# ["13"]["inputs"]["lora_name_4"] = 
+# ["13"]["inputs"]["model_weight_4"] = 
+# # cn res
+# ["14"]["inputs"]["value"] = 
+# # cn 0
+# ["15"]["inputs"]["control_net_name"] = 
+# ["16"]["inputs"]["preprocessor"] = 
+# ["23"]["inputs"]["controlnet_strength_0"] = 
+# # cn 1
+# ["17"]["inputs"]["control_net_name"] = 
+# ["18"]["inputs"]["preprocessor"] = 
+# ["23"]["inputs"]["controlnet_strength_1"] = 
+# # cn 2
+# ["19"]["inputs"]["control_net_name"] = 
+# ["20"]["inputs"]["preprocessor"] = 
+# ["23"]["inputs"]["controlnet_strength_2"] = 
+# # cn 3
+# ["21"]["inputs"]["control_net_name"] = 
+# ["22"]["inputs"]["preprocessor"] = 
+# ["23"]["inputs"]["controlnet_strength_3"] = 
